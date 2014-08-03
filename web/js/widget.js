9f6d11aeca97edbf0b16bca81b3ad5b57f04261c
maciek-kemnitz <maciek.kemnitz@gmail.com>
2014-08-03 16:12:51 +0200

    request

--- web/js/widget.js
+++ web/js/widget.js
@@ -18,7 +18,7 @@ $(function () {
             $("<style type='text/css'> .modalMargin{ margin-left:" + modalLeftMargin + "px;}</style>").appendTo("head");
 
 	        var form = $('form[method="post"]').first();
-	        $(form).find('input[type="submit"]').attr('id', 'addToCart');
+	        $(form).find('button[type="submit"]').attr('id', 'addToCart');
 
             $('body').delegate('#addToCart','click', function () {
 
