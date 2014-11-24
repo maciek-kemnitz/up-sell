b9a949237725d55f8a03fc7842ffa876c72a7290
maciekkemnitz <maciek.kemnitz@gmail.com>
2014-11-25 00:34:11 +0100

    well

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -67,7 +67,7 @@ class WebhookController implements ControllerProviderInterface
 
 			$productData = $request->request->get('product');
 
-			if ($request->headers->get('shoplo-shop-id') == 10344)
+			if ((int) $request->headers->get('shoplo-shop-id') == 10344)
 			{
 				$tmpRequest = new TmpRequest();
 				$tmpRequest->setData(json_encode($productData));
